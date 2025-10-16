7dc646ac39bea71d4c588f30382b8e4463e349b7
quevedoana <et.quevedoana@gmail.com>
2025-10-16 19:35:35 -0300

    clase cliente y conexion lista

---
+++ src/Modelo/Cliente.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Anitabonita
+ */
+public class Cliente {
+    private int codCli=-1;
+    private int dni;
+    private String NombreCompleto;
+    private int telefono;
+    private int edad;
+    private String afecciones;
+    private boolean estado;
+
+    public Cliente() {
+    }
+
+    public Cliente(int dni, String NombreCompleto, int telefono, int edad, String afecciones, boolean estado) {
+        this.dni = dni;
+        this.NombreCompleto = NombreCompleto;
+        this.telefono = telefono;
+        this.edad = edad;
+        this.afecciones = afecciones;
+        this.estado = estado;
+    }
+
+    public int getCodCli() {
+        return codCli;
+    }
+
+    public void setCodCli(int codCli) {
+        this.codCli = codCli;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombreCompleto() {
+        return NombreCompleto;
+    }
+
+    public void setNombreCompleto(String NombreCompleto) {
+        this.NombreCompleto = NombreCompleto;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getAfecciones() {
+        return afecciones;
+    }
+
+    public void setAfecciones(String afecciones) {
+        this.afecciones = afecciones;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
