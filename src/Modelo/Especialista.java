815b71dfbf6d4a5165d1c378c90db6269a2e8e39
AntonioAssat <turcotomas280302@gmail.com>
2025-10-21 20:58:11 -0300

    Agregue clase Especialista contructor y metodos

--- src/Modelo/Especialista.java
+++ src/Modelo/Especialista.java
@@ -10,5 +10,79 @@ package Modelo;
  * @author Anitabonita
  */
 public class Especialista {
+    //Masajista: matricula, nombre y ape, teléfono, especialidad (facial, corporal, relajación, o estético), estado
+    private String matricula;
+    private String nombre;
+    private String apellido;
+    private int telefono;
+    private String especialidad;
+    private boolean estado;
+
+    public Especialista(String matricula, String nombre, String apellido, int telefono, String especialidad, boolean estado) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.especialidad = especialidad;
+        this.estado = estado;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Especialista: " + "matricula=" + matricula + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono + ", especialidad=" + especialidad + ", estado=" + estado;
+    }
+
+    public String getMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
--- src/Persistencia/EspecialistaData.java
+++ src/Persistencia/EspecialistaData.java
@@ -5,10 +5,49 @@
  */
 package Persistencia;
 
-/**
- *
- * @author Anitabonita
- */
+import Modelo.Conexion;
+import Modelo.Especialista;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+
 public class EspecialistaData {
-    
+    private Connection conexion = null;
+
+    public EspecialistaData() {
+        this.conexion = Conexion.getConexion();
+    }
+    public void guardarEspecialista(Especialista e) {
+        try {
+            //Masajista: matricula, nombre y ape, teléfono, especialidad (facial, corporal, relajación, o estético), estado
+            String sql = "INSERT INTO Especialidad (Matricula,Nombre,Apellido,Telefono,Especialidad,Estado) VALUES (?,?,?,?,?,?);";
+            PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, e.getMatricula());
+            ps.setString(2, e.getNombre());
+            ps.setString(3, e.getApellido());
+            ps.setInt(4, e.getTelefono());
+            ps.setString(3, e.getEspecialidad());
+            ps.setBoolean(6, e.isEstado());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+               // e.setIdInscripto(rs.getInt(1));
+               e.setMatricula(rs.getString(1));
+                JOptionPane.showMessageDialog(null, "Especialista Registrado");
+
+            } else {
+                JOptionPane.showMessageDialog(null, "No se pudo obtener la matricula del especialista");
+                ps.close();
+            }
+            
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al guardar el especialista" + e.getMessage());
+        }
+    }
 }
