5069b01f04584e198ee56a4a23f956fe8d25d52a
Candela Naranjo <candenaranjo05@gmail.com>
2025-10-21 19:33:12 -0300

    clase Tratamiento

--- src/Modelo/Tratamiento.java
+++ src/Modelo/Tratamiento.java
@@ -5,10 +5,95 @@
  */
 package Modelo;
 
+import java.util.List;
+
 /**
  *
- * @author Anitabonita
+ * @author Candela Naranjo
  */
 public class Tratamiento {
+
+    private int codTratam;
+    private String nombre;
+    private String detalle;
+    private int duracion;
+    private double costo;
+    private boolean activo;
+    private List<String> productos;
+
+    public Tratamiento() {
+    }
+
+    public Tratamiento(int codTratam, String nombre, String detalle, int duracion, double costo, boolean activo, List<String> productos) {
+        this.codTratam = codTratam;
+        this.nombre = nombre;
+        this.detalle = detalle;
+        this.duracion = duracion;
+        this.costo = costo;
+        this.activo = activo;
+        this.productos = productos;
+    }
+
+    public int getCodTratam() {
+        return codTratam;
+    }
+
+    public void setCodTratam(int codTratam) {
+        this.codTratam = codTratam;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public List<String> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<String> productos) {
+        this.productos = productos;
+    }
+
+    @Override
+    public String toString() {
+        return "Tratamiento{" + "codTratam=" + codTratam + ", nombre=" + nombre + ", detalle=" + detalle + ", duracion=" + duracion + ", costo=" + costo + ", activo=" + activo + ", productos=" + productos + '}';
+    }
     
+
 }
