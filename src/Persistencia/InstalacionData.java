de1e013ad4afc81c93bf6463a66c46b808497fe5
quevedoana <et.quevedoana@gmail.com>
2025-10-23 17:31:44 -0300

    .

--- src/Persistencia/InstalacionData.java
+++ src/Persistencia/InstalacionData.java
@@ -5,11 +5,161 @@
  */
 package Persistencia;
 
+import Modelo.Cliente;
+import Modelo.Conexion;
+import Modelo.Instalacion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Anitabonita
  */
 public class InstalacionData {
+
+    private Connection conexion = null;
+
+    public InstalacionData() {
+        conexion = Conexion.getConexion();
+    }
+
+    public void guardarInstalacion(Instalacion in) {
+        String query = "INSERT INTO instalacion (nombre, detalleDeUso, precio30m, estado) VALUES (?,?,?,?)";
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, in.getNombre());
+            ps.setString(2, in.getDetalleDeUso());
+            ps.setDouble(3, in.getPrecio30m());
+            ps.setBoolean(4, in.isEstado());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                in.setCodInstal(rs.getInt(1));
+            } else {
+                JOptionPane.showMessageDialog(null, "No se pudo obtener el id de la instalacion");
+                ps.close();
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al guardar la instalacion" + e.getMessage());
+        }
+
+    }
+
+    public Instalacion buscarInstalacion(int CodInstal) {
+        String sql = "SELECT * FROM instalacion WHERE codCli=?";
+        Instalacion ins = null;
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, CodInstal);
+            ResultSet resultado = ps.executeQuery();
+            if (resultado.next()) {
+
+                ins = new Instalacion(resultado.getString("nombre"), resultado.getString("detalleDeUso"), resultado.getDouble("precio30m"), resultado.getBoolean("estado"));
+                ins.setCodInstal(resultado.getInt("CodInstal"));
+
+            } else {
+                System.out.println("No se encontro la instalacion");
+            }
+            ps.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al buscar la instalacion" + e.getMessage());
+
+        }
+        return ins;
+    }
     
+    public List<Instalacion> ListarInstalacion() { 
+        String sql = "SELECT * FROM instalacion";
+        List<Instalacion> instalacion = new ArrayList ();
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ResultSet resultado = ps.executeQuery();
+            while (resultado.next()) {
+
+                Instalacion ins = new Instalacion(resultado.getString("nombre"), resultado.getString("detalleDeUso"), resultado.getDouble("precio30m"), resultado.getBoolean("estado"));
+                ins.setCodInstal(resultado.getInt("CodInstal"));
+            }
+            ps.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al buscar la instalacion" + e.getMessage());
+
+        }
+        return instalacion;
+    }
+    public void actualizarInstalacion(Instalacion in) {
+        String query = "UPDATE instalacion SET nombre = ?, detalleDeUso = ?, precio30m = ?, estado = ? WHERE codInstal = ?";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+                ps.setString(1, in.getNombre());
+                ps.setString(2, in.getDetalleDeUso());
+                ps.setDouble(3, in.getPrecio30m());
+                ps.setBoolean(4, in.isEstado());
+                ps.setInt(5, in.getCodInstal());
+                ps.executeUpdate();
+                
+                ps.close();
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al actualizar la instalacion" + e.getMessage());
+        }
+    }
+    public void BorrarInstalacion(int codInstal) {
+        String query = "DELETE FROM instalacion WHERE codInstal = ?";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, codInstal);
+            ps.executeUpdate();
+            
+            ps.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al eliminar la instalacion" + e.getMessage());
+        }
+    }
     
+    public void HabilitarCliente(Instalacion in){
+        String query = "UPDATE instalacion SET estado = 1 WHERE codInstal = ? AND estado = 0";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+                ps.setInt(1, in.getCodInstal());
+                ps.setBoolean(2, in.isEstado());
+                ps.executeUpdate();
+                
+                ps.close();
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al habilitar la instalacion" + e.getMessage());
+        }
+    }
+    
+    public void DeshabilitarCliente(Instalacion in){
+        String query = "UPDATE cliente SET estado = 0 WHERE codInstal = ? AND estado = 1";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+                ps.setInt(1, in.getCodInstal());
+                ps.setBoolean(2, in.isEstado());
+                ps.executeUpdate();
+                
+                ps.close();
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al deshabilitar la instalacion" + e.getMessage());
+        }
+    }
+    
+
 }
--- src/Vista/VistaInstalacion.java
+++ src/Vista/VistaInstalacion.java
@@ -27,15 +27,39 @@ public class VistaInstalacion extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+
+        jLabel1.setText("jLabel1");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(203, 203, 203)
+                .addComponent(jLabel1)
+                .addContainerGap(267, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addContainerGap(378, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -43,5 +67,7 @@ public class VistaInstalacion extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
