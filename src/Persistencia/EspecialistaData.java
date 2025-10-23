c0635f8dd490a99c3d9d4949ee66fbb5ee45e0bc
AntonioAssat <turcotomas280302@gmail.com>
2025-10-23 19:27:43 -0300

    Especialista data hecho

--- src/Persistencia/EspecialistaData.java
+++ src/Persistencia/EspecialistaData.java
@@ -12,6 +12,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 
@@ -21,14 +23,14 @@ public class EspecialistaData {
     public EspecialistaData() {
         this.conexion = Conexion.getConexion();
     }
+    //Guardar Especialista
     public void guardarEspecialista(Especialista e) {
         try {
             //Masajista: matricula, nombre y ape, teléfono, especialidad (facial, corporal, relajación, o estético), estado
-            String sql = "INSERT INTO Especialidad (Matricula,Nombre,Apellido,Telefono,Especialidad,Estado) VALUES (?,?,?,?,?,?);";
+            String sql = "INSERT INTO especialista (matricula,NombreyApellido,telefono,especialidad,estado) VALUES (?,?,?,?,?,?);";
             PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, e.getMatricula());
-            ps.setString(2, e.getNombre());
-            ps.setString(3, e.getApellido());
+            ps.setString(2, e.getNombreYApellido());
             ps.setInt(4, e.getTelefono());
             ps.setString(3, e.getEspecialidad());
             ps.setBoolean(6, e.isEstado());
@@ -50,4 +52,118 @@ public class EspecialistaData {
             JOptionPane.showMessageDialog(null, "Error al guardar el especialista" + e.getMessage());
         }
     }
+    //Buscar Especialista
+    public Especialista buscarEspecialista(String matricula) { //select 1 alumno
+        String sql = "SELECT * FROM especialista WHERE matricula=?";
+        Especialista esp = null;
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setString(1, matricula);
+            ResultSet resultado = ps.executeQuery();
+            if (resultado.next()) {
+                   //Masajista: matricula, nombre y ape, teléfono, especialidad (facial, corporal, relajación, o estético), estado
+                esp = new Especialista(resultado.getString("Matricula"), resultado.getString("Nombre y Apellido"), resultado.getInt("Telefono"), resultado.getString("Especialidad"), resultado.getBoolean("Estado"));
+                esp.setMatricula(resultado.getString("Matricula"));
+
+            } else {
+                System.out.println("No se encontro el alumno");
+            }
+            ps.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al buscar el Especialista" + e.getMessage());
+
+        }
+        return esp;
+    }
+    //Listar Especialista
+    public List<Especialista> listarEspecialistas() { //select *
+        String sql = "SELECT * FROM especialista WHERE 1";
+        List<Especialista> especia = new ArrayList();
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ResultSet resultado = ps.executeQuery();
+            while (resultado.next()) {
+                Especialista esp = new Especialista(resultado.getString("Matricula"), resultado.getString("Nombre y Apellido"), resultado.getInt("Telefono"), resultado.getString("Especialidad"), resultado.getBoolean("Estado"));
+                especia.add(esp);
+
+            }
+            ps.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al listar especialista" + e.getMessage());
+        }
+
+        return especia;
+    }
+    //ACTUALIZAR Especialista
+    public void actualizarAlumno(Especialista a) {
+        String query = "UPDATE especialista SET matricula = ?, NombreYApellido = ?, telefono = ?, especialidad = ?, estado = ? WHERE idAlumno = ?";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+                ps.setString(1, a.getMatricula());
+                ps.setString(2, a.getNombreYApellido());
+                ps.setInt(3, a.getTelefono());
+                ps.setString(1, a.getEspecialidad());
+                ps.setBoolean(5, a.isEstado());
+                
+                ps.executeUpdate();
+                
+                ps.close();
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al actualizar el Especialista" + e.getMessage());
+        }
+        
+    }
+    //BORRAR Especialista
+    public void BorrarAlumno(String matricula) {
+        String query = "DELETE FROM especialista WHERE matricula = ?";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setString(1, matricula);
+            ps.executeUpdate();
+            
+            ps.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al eliminar el Especialista" + e.getMessage());
+        }
+    }
+    //ALTA LOGICA = darle estado activo a los inactivos
+    public void HabilitarAlumno(Especialista a){
+        String query = "UPDATE especialista SET estado = 1 WHERE matricula = ? AND Estado = 0";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+                ps.setString(1, a.getMatricula());
+                ps.setBoolean(2, a.isEstado());
+                ps.executeUpdate();
+                
+                ps.close();
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al habilitar el Especialista" + e.getMessage());
+        }
+    }
+    //BAJA LOGICA = darle estado inactivo a los activos
+     public void DeshabilitarAlumno(Especialista a){
+        String query = "UPDATE especialista SET estado = 0 WHERE matricula = ? AND Estado = 1";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+                ps.setString(1, a.getMatricula());
+                ps.setBoolean(2, a.isEstado());
+                ps.executeUpdate();
+                
+                ps.close();
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al deshabilitar el Especialista" + e.getMessage());
+        }
+    }
+    
 }
