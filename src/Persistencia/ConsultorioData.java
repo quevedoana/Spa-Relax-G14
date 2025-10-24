01bcc1b77aa59b188913a2eab3484a7eafc1a20f
totolayo <totorastone@gmail.com>
2025-10-24 15:33:25 -0300

    Consultorio Data
    Hice la clase Consultorio Data

--- src/Persistencia/ConsultorioData.java
+++ src/Persistencia/ConsultorioData.java
@@ -5,10 +5,158 @@
  */
 package Persistencia;
 
+import Modelo.Consultorio;
+import Modelo.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Anitabonita
  */
 public class ConsultorioData {
+    private Connection conexion = null;
     
+    public ConsultorioData() {
+        conexion = Conexion.getConexion();
+    }
+    
+    public void guardarConsultorio(Consultorio con) {
+        String query = "INSERT INTO consultorio (nroConsultorio, usos, equipamiento, apto) VALUES (?, ?, ?, ?";
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, con.getNroConsultorio());
+            ps.setString(2, con.getUsos());
+            ps.setString(3, con.getEquipamiento());
+            ps.setBoolean(4, con.isApto());
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                con.setNroConsultorio(rs.getInt(1));
+            } else {
+                JOptionPane.showMessageDialog(null, "No se pudo obtener el numero del consultorio");
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al guardar el consultorio: " + e.getMessage());
+        }
+    }
+    
+    public Consultorio buscarConsultorio(int nroConsultorio) {
+        String sql = "SELECT * FROM consultorio WHERE nroConsultorio = ?";
+        Consultorio cons = null;
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, nroConsultorio);
+            ResultSet resultado = ps.executeQuery();
+            
+            if (resultado.next()) {
+                cons = new Consultorio(
+                resultado.getInt("nroConsultorio"),
+                resultado.getString("usos"),
+                resultado.getString("equipamiento"),
+                resultado.getBoolean("apto")
+                );
+            } else {
+                System.out.println("Nose encontro el consultorio con numero " + nroConsultorio);
+        }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showConfirmDialog(null, "Error al buscar el consultorio: " + e.getMessage());
+        }
+        return cons;
+    }
+    
+    
+    public List<Consultorio> ListarConsultorios() {
+        String sql = "SELECT * FROM consultorio";
+        List<Consultorio> consultorios = new ArrayList<>();
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ResultSet resultado = ps.executeQuery();
+            
+            while (resultado.next()) {
+                Consultorio con = new Consultorio(
+                resultado.getInt("nroConsultorio"),
+                resultado.getString("usos"),
+                resultado.getString("equipamiento"),
+                resultado.getBoolean("apto")
+                );
+                consultorios.add(con);
+            }
+            
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al listar los consultorios: " + e.getMessage());
+        }
+        return consultorios;
+    }
+    
+    public void actualizarConsultorio(Consultorio con) {
+        String query = "UPDATE consultorio SET usos = ?, equipamiento = ?, apto = ? WHERE nroConsultorio = ?";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setString(1, con.getUsos());
+            ps.setString(2, con.getEquipamiento());
+            ps.setBoolean(3, con.isApto());
+            ps.setInt(4, con.getNroConsultorio());
+            ps.executeUpdate();
+            
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al actualizar el consultorio: " + e.getMessage());
+        }
+    }
+    
+    public void borrarConsultorio(int nroConsultorio) {
+        String query = "DELETE FROM consultorio WHERE nroConsultorio = ?";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, nroConsultorio);
+            ps.executeUpdate();
+            
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al eliminar el consultorio: " + e.getMessage());
+        }
+    }
+    
+    public void habilitarConsultorio(Consultorio c) {
+        String query = "UPDATE consultorio SET apto = 1 WHERE nroConsultorio = ? AND apto = 0";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, c.getNroConsultorio());
+            ps.executeUpdate();
+
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al habilitar el consultorio: " + e.getMessage());
+        }
+    }
+    
+    public void deshabilitarConsultorio(Consultorio c) {
+        String query = "UPDATE consultorio SET apto = 0 WHERE nroConsultorio = ? AND apto = 1";
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, c.getNroConsultorio());
+            ps.executeUpdate();
+
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al deshabilitar el consultorio: " + e.getMessage());
+        }
+    }
 }
--- src/Vista/VistaConsultorio.java
+++ src/Vista/VistaConsultorio.java
@@ -27,15 +27,39 @@ public class VistaConsultorio extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+
+        jLabel1.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
+        jLabel1.setText("Consultorio");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(141, 141, 141)
+                .addComponent(jLabel1)
+                .addContainerGap(141, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(0, 198, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -43,5 +67,7 @@ public class VistaConsultorio extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
