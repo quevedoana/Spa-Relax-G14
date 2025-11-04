e822b8ed4c5c4da45d6c53c4be21d785eae32fa4
quevedoana <et.quevedoana@gmail.com>
2025-11-04 20:49:01 -0300

    db actualizada

--- grpo14_spa_entrededos (6).sql
+++ grpo14_spa_entrededos (6).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-10-2025 a las 23:15:25
+-- Tiempo de generación: 05-11-2025 a las 00:48:23
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -63,6 +63,16 @@ CREATE TABLE `consultorio` (
   `apto` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `consultorio`
+--
+
+INSERT INTO `consultorio` (`nroConsultorio`, `usos`, `equipamiento`, `apto`) VALUES
+(1, 'Corporal', 'Camilla, aceites, música relajante', 1),
+(2, 'Facial', 'Sillón facial, vaporizador, lámpara lupa', 1),
+(3, 'Masajes', 'Piedras calientes, mesa de masaje, toallas', 1),
+(4, 'Depilacion', 'Calentador de cera, Espejo, Camilla regulable', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -120,7 +130,7 @@ CREATE TABLE `instalacion` (
 
 INSERT INTO `instalacion` (`codInstal`, `nombre`, `detalleDeUso`, `precio30m`, `estado`) VALUES
 (1, 'Piscina climatizada', 'Piscinas con agua a temperatura agradable, a veces equipadas', 20000, 1),
-(3, 'Hidromasaje', 'Bañeras con chorros de agua a presión para relajar los músc', 15000, 0),
+(3, 'Hidromasaje', 'Bañeras con chorros de agua a presión para relajar los músc', 15000, 1),
 (4, 'Sauna', 'Habitaciones con vapor de agua caliente que ayudan a purifi', 20000, 1),
 (5, 'Duchas de sensaciones', 'Combina diferentes temperaturas, presiones y aromas para est', 10000, 1),
 (6, 'Pediluvio', 'Baño de pies que generalmente combina agua fría y caliente p', 10000, 1),
@@ -157,9 +167,34 @@ CREATE TABLE `tratamiento` (
   `productos` varchar(40) NOT NULL,
   `duracion` int(11) NOT NULL,
   `costo` double NOT NULL,
-  `activo` tinyint(1) NOT NULL,
+  `activo` tinyint(4) NOT NULL,
   `tipo` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tratamiento`
+--
+
+INSERT INTO `tratamiento` (`codTratam`, `nombre`, `detalle`, `productos`, `duracion`, `costo`, `activo`, `tipo`) VALUES
+(1, 'Masaje Sueco', 'Terapia de relajación de cuerpo completo con presión ligera ', 'Aceite neutro', 60, 10000, 1, 'Relajacion'),
+(2, 'Masaje de tejido profundo', ': Para aliviar tensión muscular crónica y tratar contractura', 'Aceite y crema neutro', 60, 12000, 1, 'Relajacion'),
+(3, 'Masaje piedras calientes', ' Utiliza piedras calientes para calmar los músculos y el sis', 'Horno y piedras parafinadas', 60, 8000, 1, 'Relajacion'),
+(6, 'Masajes Especializados', ' Incluyen técnicas como la aromaterapia, masajes para pareja', 'Aceites y crema neutra', 90, 15000, 0, 'Relajacion'),
+(7, 'Facial básico', ' Limpieza profunda, exfoliación y mascarilla para refrescar ', 'Gua-Sha de Cuarzo Rosa, Rodillo Facial, ', 40, 9000, 1, 'Facial'),
+(8, 'Faciales avanzados', ' Pueden incluir tratamientos antienvejecimiento, microdermoa', 'Rodillos de belleza, aceite de comino ne', 35, 6000, 1, 'Facial'),
+(9, 'Microdermoabrasión', ' Exfolia la capa superficial de la piel usando puntas de dia', 'xfoliante Facial Microdermoabrasión Juni', 35, 6500, 1, 'Facial'),
+(10, 'Radiofrecuencia', ' Usa ondas de radio para generar calor en la piel, estimulan', 'Gel Oeneis conductor específico, limpiad', 40, 7500, 1, 'Facial'),
+(11, 'Ultrasonido', 'Usa ondas sonoras de alta frecuencia para limpiar la piel, p', 'Gel conductor de baja viscosidad', 25, 8000, 1, 'Facial'),
+(12, 'Luz Pulsada Intensa (IPL)', ' Utilizada para fotodepilación, rejuvenecimiento de la piel ', ' Gel conductor o un gel frío, protector ', 35, 5500, 1, 'Facial'),
+(13, 'Vaporizacion facial', 'Produce vapor para abrir los poros y limpiar la piel profund', 'Aceites esenciales, infusiones o hierbas', 20, 4500, 1, 'Facial'),
+(14, 'Exfoliacion corporal', 'Elimina las células muertas de la piel con el uso de sales o', ' ingredientes abrasivos: sal, azúcar o p', 45, 9000, 1, 'Corporal'),
+(15, 'Envolturas corporales', ' Hidratan y desintoxican la piel con ingredientes como algas', ' Algas, barro o chocolate', 60, 20000, 1, 'Corporal'),
+(16, 'Hidroterapia', 'Sesiones en saunas, jacuzzis o baños de vapor.', 'Toallon', 50, 25000, 1, 'Corporal'),
+(17, 'Vacumterapia', 'Es una succión corporal que mejora la circulación sanguínea ', 'Aceites a base de parafina o aceites veg', 40, 17000, 1, 'Corporal'),
+(18, 'Drenaje linfático', 'Estimular la circulación del sistema linfático para eliminar', 'Crema neutra', 70, 11000, 1, 'Estetico'),
+(19, ' Masaje reafirmante', ' Mejorar la firmeza y la elasticidad de la piel, a menudo en', 'Crema neutra', 80, 12000, 1, 'Estetico'),
+(20, 'Masaje modelador', 'Reducir medidas, moldear la silueta y combatir la celulitis.', 'Crema neutra', 60, 12000, 1, 'Estetico'),
+(21, 'Masaje Reductor', ' Utiliza técnicas firmes para romper células de grasa, mejor', 'Aceites (de almendras, coco) o cremas co', 60, 17000, 1, 'Estetico');
 
 --
 -- Índices para tablas volcadas
@@ -229,7 +264,7 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de la tabla `consultorio`
 --
 ALTER TABLE `consultorio`
-  MODIFY `nroConsultorio` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `nroConsultorio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `dia_de_spa`
@@ -248,6 +283,12 @@ ALTER TABLE `instalacion`
 --
 ALTER TABLE `sesion`
   MODIFY `codSesion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  MODIFY `codTratam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Restricciones para tablas volcadas
